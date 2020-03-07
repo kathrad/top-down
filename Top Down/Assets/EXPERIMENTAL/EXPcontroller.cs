d00d127ba430a89ea2be0d3b6d3009fad9c791fe
Nikita Kazakov <nkazakov0@icloud.com>
2020-03-07 22:07:51 +0300

    State Machines

---
+++ Top Down/Assets/EXPERIMENTAL/EXPcontroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXPcontroller : MonoBehaviour
+{
+	public Animator anim;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			anim.SetTrigger("Move");
+		else if (Input.GetKeyDown(KeyCode.F))
+			anim.SetTrigger("Attack");
+		else if (Input.GetKeyDown(KeyCode.E))
+			anim.SetTrigger("Interact");
+		else if (Input.GetKeyDown(KeyCode.H))
+			anim.SetTrigger("Death");
+		else if (Input.GetKeyDown(KeyCode.R))
+			anim.SetTrigger("Ressurect");
+	}
+}
